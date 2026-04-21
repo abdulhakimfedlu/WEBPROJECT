f656aa1f9fd2294ab6dacfa568ba3e56e3dbece1
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2026-04-21 20:34:12 +0300

    Update admin_settings.php

--- admin_settings.php
+++ admin_settings.php
@@ -13,7 +13,6 @@ $admin_id = $_SESSION['admin_id'];
 $action = $_POST['action'] ?? $_GET['action'] ?? '';
 
 
-
 // List all admins
 if ($action === 'list') {
     $result = $conn->query('SELECT id, email, created_at FROM admins ORDER BY id ASC');
